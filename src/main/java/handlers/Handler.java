47bc76f60559a78f62b66039a6f27e0cf2782952
Himalee <himalee.tailor@gmail.com>
2019-05-29 19:21:08 +0100

    Refactor: create methodNotAllowedResponse()

--- src/main/java/handlers/Handler.java
+++ src/main/java/handlers/Handler.java
@@ -12,12 +12,15 @@ public abstract class Handler {
         boolean requestContainsValidHttpMethod = httpMethods().contains(request.getHttpMethod());
         if (requestContainsValidHttpMethod) {
             return buildResponse(request);
-        } else {
-            return new ResponseBuilder().setStatusCode("HTTP/1.1 405 Method Not Allowed\r\n").setHeaders("Allow: " + String.join(", ", this.httpMethods()) + "\r\n\r\n").setBody("").build();
         }
+        return methodNotAllowedResponse();
     }
 
     public abstract String url();
     public abstract List<String> httpMethods();
     public abstract Response buildResponse(Request request);
+
+    private Response methodNotAllowedResponse() {
+        return new ResponseBuilder().setStatusCode("HTTP/1.1 405 Method Not Allowed\r\n").setHeaders("Allow: " + String.join(", ", this.httpMethods()) + "\r\n\r\n").setBody("").build();
+    }
 }
