fef79e2693bf67bd35a4b51452e3e40349ff437e
Himalee <himalee.tailor@gmail.com>
2019-05-29 19:14:21 +0100

    Update: return 404 to request url that has not been set up

--- src/test/java/server/RouteHandlerTest.java
+++ src/test/java/server/RouteHandlerTest.java
@@ -7,11 +7,23 @@ import server.request.RequestParser;
 import server.response.Response;
 
 public class RouteHandlerTest {
+
+    RouteHandler routeHandler;
+
+    public Response routeRequestAndGetResponseFor(Request request) {
+        routeHandler = new RouteHandler();
+        return routeHandler.getResponse(request);
+    }
+
     @Test
-    public void simpleGetRequestBuildResponse() {
+    public void simpleGetRequestBuild200Response() {
         Request request = new RequestParser("GET /simple_get HTTP/1.1\n\n").buildRequest();
-        RouteHandler routeHandler = new RouteHandler();
-        Response response = routeHandler.getResponse(request);
-        Assert.assertEquals("HTTP/1.1 200 OK\r\n\r\n", response.format());
+        Assert.assertEquals("HTTP/1.1 200 OK\r\n\r\n", routeRequestAndGetResponseFor(request).format());
+    }
+
+    @Test
+    public void randomUrlBuild404Response() {
+        Request request = new RequestParser("GET /random_url HTTP/1.1\n\n").buildRequest();
+        Assert.assertEquals("HTTP/1.1 404 Not Found\r\n\r\n", routeRequestAndGetResponseFor(request).format());
     }
 }
